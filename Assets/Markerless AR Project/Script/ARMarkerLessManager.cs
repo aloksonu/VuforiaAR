b8e4365e1b44eb45adecd9a7650aff0181be988b
Alok Dubey <alok.dubey@seamedu.com>
2025-04-08 09:40:43 +0530

    AR Car Added

--- Assets/Markerless AR Project/Script/ARMarkerLessManager.cs
+++ Assets/Markerless AR Project/Script/ARMarkerLessManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class ARMarkerLessManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject car;
+    public float speed = 5f; // Car movement speed
+    private bool isMoving = false;
+    private Vector3 initialPosition; // Store the initial position
+    public AudioSource carAudio; // Reference to the AudioSource component
+
     void Start()
     {
-        
+        initialPosition = car.transform.position;  // Save the starting position
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving)
+        {
+            MoveCar();
+        }
+    }
+
+    public void OnTouchStart()
+    {
+        isMoving = true;
+        if (carAudio != null && !carAudio.isPlaying)
+        {
+            carAudio.Play();  // Play sound when touch starts
+        }
+    }
+
+    public void OnTouchEnd()
+    {
+        isMoving = false;
+        if (carAudio != null && carAudio.isPlaying)
+        {
+            carAudio.Stop();  // Stop sound when touch ends
+        }
+    }
+
+    void MoveCar()
+    {
+        car.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void OnTargetFound()
+    {
+
+    }
+    public void OnTargetLost()
+    {
+        car.transform.position = initialPosition;
+        carAudio.Stop();  // Stop sound when touch ends
     }
 }
